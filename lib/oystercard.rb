dad40c47cc0a55b0ebac728232fc532fd56e1786
Hannah Desmond <desmond.hannah@googlemail.com>
2021-02-08 16:34:50 +0000

    Add test to raise error if limit is exceeded.

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,15 @@
+LIMIT = 90
+
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :limit
 
   def initialize
     @balance = 0
+    @limit = LIMIT
   end
   def top_up(value)
+    projection = @balance + value
+    raise 'top up limit exceeded' if projection > @limit
     @balance += value
   end
-
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,11 +4,17 @@ describe Oystercard do
   it 'has a balance of 0 by default' do
     expect(subject.balance).to eq(0)
   end
+  it 'has a limit' do
+    expect(subject.limit).to eq(90)
+  end
 
   describe '#top_up' do
     it 'should take a top-up value and add it to the card balance' do
       subject.top_up(10)
       expect(subject.balance).to eq (10)
+    end
+    it 'should raise an error if top_up returns more than 90' do
+      expect { subject.top_up(91) }.to raise_error('top up limit exceeded')
     end
   end
 end
